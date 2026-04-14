9413c22201600773bf85ebf505fce82d8e45eee9
skaua25-bit <s.kaua25@escola.pr.gov.br>
2026-04-14 11:47:39 +0000

    trabalho 2 finalizado

---
+++ livroslegais.js
@@ -0,0 +1,20 @@
+const precoLivros = [ 
+    {titulo: "totorial",
+    preco: 25},
+
+    {titulo: "comoNaoTerTrabalho",
+   preco: 15
+ },
+   {titulo: "ComoFugirDaex",
+ preco: 30
+     },
+     {titulo: "fatimaBermadi",
+      preco: 50
+ },
+ {titulo: "ComoFalaRusso",
+ preco:45
+},
+{titulo: "jabulanya",
+preco:20}]; 
+
+module.exports = precoLivros;
